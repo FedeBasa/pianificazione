ec8178f40d6c095e433371dfa4e5b23d487f2199
Daniele D'Ambrosi <daniele.dambrosi@soprasteria.com>
2016-03-15 17:52:01 +0100

    correzione commit precedente

--- v2/src/main/java/it/soprasteria/pianificazione/v2/bean/upload/UploadEmployeeBean.java
+++ v2/src/main/java/it/soprasteria/pianificazione/v2/bean/upload/UploadEmployeeBean.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package it.soprasteria.pianificazione.v2.bean.upload;
 
 import org.springframework.web.multipart.MultipartFile;
@@ -16,22 +15,3 @@ public class UploadEmployeeBean {
 	}
 
 }
-=======
-package it.soprasteria.pianificazione.v2.bean.upload;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public class UploadEmployeeBean {
-
-	private MultipartFile file;
-
-	public MultipartFile getFile() {
-		return file;
-	}
-
-	public void setFile(MultipartFile file) {
-		this.file = file;
-	}
-
-}
->>>>>>> b6ddb4b0c76942c9db1b95b820af63a7a000fc48
