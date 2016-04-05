bbe1ff8cd5720b4247da026bda5ec3c7ff079f80
FedeBasa <f.basanese@live.com>
2016-04-05 16:33:44 +0200

    fix pagina pm

--- v2/src/main/java/it/soprasteria/pianificazione/v2/service/PmService.java
+++ v2/src/main/java/it/soprasteria/pianificazione/v2/service/PmService.java
@@ -1,20 +1,19 @@
 package it.soprasteria.pianificazione.v2.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import it.soprasteria.pianificazione.v2.bean.UserBean;
+import it.soprasteria.pianificazione.v2.bean.PmBean;
 import it.soprasteria.pianificazione.v2.dao.PmDao;
 public class PmService {
 	
 	@Autowired
 	PmDao dao;
 	
-		public List<UserBean> getStatus(Integer month){
-			
-			return dao.verifyV2(month);
-			
+		public List<PmBean> getStatus(Integer month){
+			 return dao.verifyStatus(month, dao);
 		}
 	
 }
